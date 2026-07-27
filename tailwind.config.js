609ca5d502137ac3eb886dc931afb572193f277b
Bhupendra Patil <bhupendrarajput1232@gmail.com>
2026-07-27 16:17:20 +0530

    feat: update portfolio logo and favicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: "class",
-    content: [
-      "./index.html",
-      "./src/**/*.{js,jsx,ts,tsx}",
-    ],
-    theme: {
-      extend: {
-        colors: {
-          purple: "#a855f7",
-          "dark-100": "#1a1a1a",
-          "dark-300": "#121212",
-        },
-        boxShadow: {
-          soft: "0 4px 24px rgba(0,0,0,0.2)",
-        },
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        // purple: "#a855f7",
+        // "dark-100": "#1a1a1a",
+        // "dark-300": "#121212",
+      },
+      boxShadow: {
+        soft: "0 4px 24px rgba(0,0,0,0.2)",
       },
     },
-    plugins: [],
-  };
-  
+  },
+  plugins: [],
+};
